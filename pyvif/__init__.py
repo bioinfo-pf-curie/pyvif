13a0a55c4cbfe83948ffc5d424e30ea3c70dfcbf
ddesvillechabrol <dimitri.desvillechabrol@curie.fr>
2018-06-21 14:11:44 +0200

    add __init__

---
+++ pyvif/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Python Virus Integration Finder."""
+
+__author__ = """Dimitri Desvillechabrol"""
+__email__ = 'dimitri.desvillechabrol@curie.fr'
+__version__ = '0.1.0'
