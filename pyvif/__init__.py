ae752c3ef711dbb64430011b79bbc3671036dfc9
ddesvillechabrol <dimitri.desvillechabrol@curie.fr>
2018-06-22 19:09:03 +0200

    add logger

--- pyvif/__init__.py
+++ pyvif/__init__.py
@@ -5,3 +5,19 @@
 __author__ = """Dimitri Desvillechabrol"""
 __email__ = 'dimitri.desvillechabrol@curie.fr'
 __version__ = '0.1.0'
+
+# colnames of PAF files
+_PAF_COLNAMES = ['chr', 'r_start', 'r_end', 'mapq', 'strand', 'q_name',
+                 'q_length', 'q_start', 'q_end']
+
+# add logger
+import logging
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(
+    "%(asctime)s - %(levelname)-8s %(message)s",
+    "%Y-%m-%d %H:%M:%S"
+))
+logger = logging.getLogger(__name__)
+logger.setLevel('INFO')
+logger.addHandler(handler)
